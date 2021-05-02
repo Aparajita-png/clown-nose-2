2e67d10580126e33958b59843ca1344876a10a73
Aparajita-png <77184488+Aparajita-png@users.noreply.github.com>
2021-05-02 10:38:09 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,10 +5,9 @@ video=createCapture(VIDEO);
 video.size(300,300);
 video.hide();
 poseNet=ml5.poseNet(video,modelLoaded);
-poseNet.on('pose',gotPoses);
+poseNet.on('pose',gotPoses);}
 function modelLoaded(){
  console.log('modelLoaded');
-}
 }
 function draw(){
 image(video,0,0,300,300);
@@ -19,9 +18,9 @@ stroke(255,0,0);
 function gotPoses(results){
 if(results.length>0){
 console.log(results);
-noseX=results[0].pose.nose.X;
-noseY=results[0].pose.nose.Y;
-console.log("noseX="+results[0].pose.nose.X);
-console.log("noseY="+results[0].pose.nose.Y);
+noseX=results[0].pose.nose.x;
+noseY=results[0].pose.nose.y;
+console.log("noseX="+noseX);
+console.log("noseY="+noseY);
 }
 }
